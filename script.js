001621bea2e1591d6100f787a2c6b93b445987dc
Alexis <alexischecura@gmail.com>
2022-09-09 23:54:16 -0300

    finishing

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 const grids = document.querySelector(".grids");
 const grid = document.createElement("div");
-const colorSelector = document.querySelector(".colorSelector");
 
-let size = 16;
-let lado = 640 / size;
-grid.classList.add("grid");
-grid.style.width = lado + "px";
-grid.style.height = lado + "px";
-grid.style.backgroundColor = 'rgb(255, 255, 255)'
+let sizeGridDefault = 16;
 
-const createGrid = (size) => {
-  grids.innerHTML = ''
-  for (let j = 0; j < size; j++) {
-    for (let i = 0; i < size; i++) {
+const createGrid = (sizeGrid) => {
+  lado = 640 / sizeGrid;
+  grids.innerHTML = "";
+  grid.classList.add("grid");
+  grid.style.width = (lado)+ "px";
+  grid.style.height = lado + "px";
+  grid.style.backgroundColor = "rgb(255, 255, 255)";
+  for (let j = 0; j < sizeGrid; j++) {
+    for (let i = 0; i < sizeGrid; i++) {
       grids.appendChild(grid.cloneNode(true));
     }
   }
+  addEvents();
 };
-createGrid(size);
+createGrid(sizeGridDefault);
+
+let sizeGrid = sizeGridDefault;
+
+const sizeBigger = document.querySelector(".sizeBigger");
+const sizeSmaller = document.querySelector(".sizeSmaller");
+const sizeDom = document.querySelector(".sizeDom");
+sizeBigger.addEventListener("click", () => {
+  if (sizeGrid === 64) return;
+  createGrid(++sizeGrid);
+  sizeDom.innerText = sizeGrid + " X " + sizeGrid;
+});
+
+sizeSmaller.addEventListener("click", () => {
+  if (sizeGrid === 1) return;
+  createGrid(--sizeGrid);
+  sizeDom.innerText = sizeGrid + " X " + sizeGrid;
+});
+
+const colorSelector = document.querySelector(".colorSelector");
 
 let color = colorSelector.value;
 colorSelector.addEventListener(
@@ -47,10 +66,48 @@ colorBlack.addEventListener("click", () => {
   rainbowMode = false;
   shaddingMode = false;
 });
-const rubber = document.querySelector(".rubber");
-rubber.addEventListener("click", () => {
+const colorWhite = document.querySelector(".colorWhite");
+colorWhite.addEventListener("click", () => {
   colorSelector.value = "#FFFFFF";
   color = colorSelector.value;
+  shaddingBtn.classList.remove("selected");
+  rainbowBtn.classList.remove("selected");
+  rainbowMode = false;
+  shaddingMode = false;
+});
+const colorRed = document.querySelector(".colorRed");
+colorRed.addEventListener("click", () => {
+  colorSelector.value = "#FF0000";
+  color = colorSelector.value;
+  shaddingBtn.classList.remove("selected");
+  rainbowBtn.classList.remove("selected");
+  rainbowMode = false;
+  shaddingMode = false;
+});
+const colorGreen = document.querySelector(".colorGreen");
+colorGreen.addEventListener("click", () => {
+  colorSelector.value = "#00FF00";
+  color = colorSelector.value;
+  shaddingBtn.classList.remove("selected");
+  rainbowBtn.classList.remove("selected");
+  rainbowMode = false;
+  shaddingMode = false;
+});
+const colorBlue = document.querySelector(".colorBlue");
+colorBlue.addEventListener("click", () => {
+  colorSelector.value = "#0000FF";
+  color = colorSelector.value;
+  shaddingBtn.classList.remove("selected");
+  rainbowBtn.classList.remove("selected");
+  rainbowMode = false;
+  shaddingMode = false;
+});
+const colorYellow = document.querySelector(".colorYellow");
+colorYellow.addEventListener("click", () => {
+  colorSelector.value = "#FFFF00";
+  color = colorSelector.value;
+  shaddingBtn.classList.remove("selected");
+  rainbowBtn.classList.remove("selected");
   rainbowMode = false;
   shaddingMode = false;
 });
@@ -59,13 +116,17 @@ rubber.addEventListener("click", () => {
 let r = 0;
 let g = 0;
 let b = 0;
-let levelOfShading = 0;
 
 let shaddingMode = false;
 const shaddingBtn = document.querySelector(".shadding");
 shaddingBtn.addEventListener("click", () => {
   shaddingMode = !shaddingMode;
-  levelOfShading = 250;
+  if (shaddingMode) shaddingBtn.classList.add("selected");
+  else {
+    shaddingBtn.classList.remove("selected")
+    color = colorSelector.value;
+  }
+  rainbowBtn.classList.remove("selected");
   rainbowMode = false;
 });
 
@@ -73,10 +134,16 @@ let rainbowMode = false;
 const rainbowBtn = document.querySelector(".rainbow");
 rainbowBtn.addEventListener("click", () => {
   rainbowMode = !rainbowMode;
+  if (rainbowMode) rainbowBtn.classList.add("selected");
+  else {
+    rainbowBtn.classList.remove("selected");
+    color = colorSelector.value;
+  }
+  shaddingBtn.classList.remove("selected");
   shaddingMode = false;
 });
 
-const rgbToArray = rgbStr => {
+const rgbToArray = (rgbStr) => {
   return rgbStr.match(/\d+/g).map(Number);
 };
 
@@ -87,29 +154,30 @@ function changeBGC() {
     b = Math.round(Math.random() * 255);
     color = `rgb(${r},${g},${b})`;
   } else if (shaddingMode) {
-    if (levelOfShading > 0) {
-      let actualColor = rgbToArray(this.style.backgroundColor) 
-      console.log(actualColor)
-      color = `rgb(${actualColor[0]-=25},${actualColor[1]-=25},${actualColor[2]-=25})`;
-    }
+    let actualColor = rgbToArray(this.style.backgroundColor);
+    color = `rgb(${(actualColor[0] -= 25)},${(actualColor[1] -= 25)},${(actualColor[2] -= 25)})`;
   }
   this.style.backgroundColor = color;
 }
 
 const boxs = document.querySelectorAll(".grid");
-const erase = document.querySelector('.erase')
+const erase = document.querySelector(".erase");
+const eraseAll = () => {
+  let boxs = document.querySelectorAll(".grid");
+  boxs.forEach((box) => (box.style.backgroundColor = "rgb(255,255,255)"));
+};
 
-const eraseAll = () => boxs.forEach((box) => box.style.backgroundColor = 'rgb(255,255,255)')
+erase.addEventListener("click", eraseAll, false);
 
-erase.addEventListener('click', eraseAll, false)
-
-boxs.forEach((box) => box.addEventListener("mousedown", changeBGC, false));
-
-document.addEventListener("mousedown", (e) => {
-  boxs.forEach((box) => box.addEventListener("mouseover", changeBGC, false));
-});
-document.addEventListener("mouseup", (e) => {
-  boxs.forEach((box) => box.removeEventListener("mouseover", changeBGC, false));
-});
-
-//Erase
+function addEvents() {
+  let boxs = document.querySelectorAll(".grid");
+  boxs.forEach((box) => box.addEventListener("mousedown", changeBGC, false));
+  document.addEventListener("mousedown", (e) => {
+    boxs.forEach((box) => box.addEventListener("mouseover", changeBGC, false));
+  });
+  document.addEventListener("mouseup", (e) => {
+    boxs.forEach((box) =>
+      box.removeEventListener("mouseover", changeBGC, false)
+    );
+  });
+}
